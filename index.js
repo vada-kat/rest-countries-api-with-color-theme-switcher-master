701db83b0f21c969e14268d3aef0124e0b871caf
dash <dark@fresh.com>
2024-05-13 16:00:36 +0300

    stage changes

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 fetch("./data.json")
-      .then(res=>res.json)
-      .then(allcountries=>{
-        rendercountries(allcountries)
-      })
-      .catch(err=>{
-        console.log(err)
-      })
+  .then((res) => res.json())
+  .then((allcountries) => {
+    renderCountries(allcountries);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-      const allcountriesDiv =document.getElementById("all-countries")
-      function rendercountries(countries){
-        allcountriesDiv.innerHTML =""
-        countries.forEach(country=>{
-            let countryDiv =document.createElement("div")
-            countryDiv.innerHTML = `
-              <img src=${} alt ${}>
+const allcountriesDiv = document.getElementById("all-countries");
+function renderCountries(countries) {
+  allcountriesDiv.innerHTML = "";
+  countries.forEach((country) => {
+    let countryDiv = document.createElement("div");
+    countryDiv.innerHTML = `
+              <img src=${country.flags.png} alt ${country.name + "flag"}>
               <h2>Name: ${country.name}</h2>
               <P>population: ${country.population} </P>
               `;
-        allcountriesDiv.append(countryDiv);      
-
-        });       
-      }
+    allcountriesDiv.append(countryDiv);
+  });
+}
